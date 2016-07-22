4d17f735854de9ec3280c16e45c90c52437318e8
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-07-22 18:05:56 +0200

    Added defines for STM LSM6DS33 accelerometer/gyroscope and LIS3MDL magnetometer

--- RTIMULib/IMUDrivers/RTIMUDefs.h
+++ RTIMULib/IMUDrivers/RTIMUDefs.h
@@ -44,6 +44,7 @@
 #define RTIMU_TYPE_BMX055                   9                   // Bosch BMX055
 #define RTIMU_TYPE_BNO055                   10                  // Bosch BNO055
 #define RTIMU_TYPE_MPU9255                  11                  // InvenSense MPU9255
+#define RTIMU_TYPE_LSM6DS33LIS3MDL          12                  // STM LSM6DS33/LIS3MDL (Pololu AltIMU-10v5)
 
 //----------------------------------------------------------
 //
@@ -382,7 +383,7 @@
 #define L3GD20_SAMPLERATE_380   2
 #define L3GD20_SAMPLERATE_760   3
 
-//  Gyro banwidth defines
+//  Gyro bandwidth defines
 
 #define L3GD20_BANDWIDTH_0     0
 #define L3GD20_BANDWIDTH_1     1
@@ -457,7 +458,7 @@
 #define L3GD20H_SAMPLERATE_400  5
 #define L3GD20H_SAMPLERATE_800  6
 
-//  Gyro banwidth defines
+//  Gyro bandwidth defines
 
 #define L3GD20H_BANDWIDTH_0     0
 #define L3GD20H_BANDWIDTH_1     1
@@ -722,7 +723,7 @@
 #define LSM9DS0_GYRO_SAMPLERATE_380 2
 #define LSM9DS0_GYRO_SAMPLERATE_760 3
 
-//  Gyro banwidth defines
+//  Gyro bandwidth defines
 
 #define LSM9DS0_GYRO_BANDWIDTH_0    0
 #define LSM9DS0_GYRO_BANDWIDTH_1    1
@@ -929,7 +930,7 @@
 #define LSM9DS1_GYRO_SAMPLERATE_476     4
 #define LSM9DS1_GYRO_SAMPLERATE_952     5
 
-//  Gyro banwidth defines
+//  Gyro bandwidth defines
 
 #define LSM9DS1_GYRO_BANDWIDTH_0    0
 #define LSM9DS1_GYRO_BANDWIDTH_1    1
@@ -1210,5 +1211,199 @@
 //  Power modes
 
 #define BNO055_PWR_MODE_NORMAL      0x00
+
+//----------------------------------------------------------
+//
+//  LSM6DS33
+
+//  I2C Slave Addresses
+
+#define LSM6DS33_ADDRESS0           0x6a
+#define LSM6DS33_ADDRESS1           0x6b
+#define LSM6DS33_ID                 0x69
+
+//  LSM6DS33 Register map
+#define LSM6DS33_FUNC_CFG_ACCESS    0x01
+
+#define LSM6DS33_FIFO_CTRL1         0x06
+#define LSM6DS33_FIFO_CTRL2         0x07
+#define LSM6DS33_FIFO_CTRL3         0x08
+#define LSM6DS33_FIFO_CTRL4         0x09
+#define LSM6DS33_FIFO_CTRL5         0x0a
+
+#define LSM6DS33_ORIENT_CFG_G       0x0b
+
+#define LSM6DS33_INT1_CTRL          0x0d
+#define LSM6DS33_INT2_CTRL          0x0e
+#define LSM6DS33_WHO_AM_I           0x0f
+#define LSM6DS33_CTRL1_XL           0x10
+#define LSM6DS33_CTRL2_G            0x11
+#define LSM6DS33_CTRL3_C            0x12
+#define LSM6DS33_CTRL4_C            0x13
+#define LSM6DS33_CTRL5_C            0x14
+#define LSM6DS33_CTRL6_C            0x15
+#define LSM6DS33_CTRL7_G            0x16
+#define LSM6DS33_CTRL8_XL           0x17
+#define LSM6DS33_CTRL9_XL           0x18
+#define LSM6DS33_CTRL10_C           0x19
+
+#define LSM6DS33_WAKE_UP_SRC        0x1b
+#define LSM6DS33_TAP_SRC            0x1c
+#define LSM6DS33_D6D_SRC            0x1d
+
+#define LSM6DS33_STATUS_REG         0x1e
+
+#define LSM6DS33_OUT_TEMP_L         0x20
+#define LSM6DS33_OUT_TEMP_H         0x21
+#define LSM6DS33_OUTX_L_G           0x22
+#define LSM6DS33_OUTX_H_G           0x23
+#define LSM6DS33_OUTY_L_G           0x24
+#define LSM6DS33_OUTY_H_G           0x25
+#define LSM6DS33_OUTZ_L_G           0x26
+#define LSM6DS33_OUTZ_H_G           0x27
+#define LSM6DS33_OUTX_L_XL          0x28
+#define LSM6DS33_OUTX_H_XL          0x29
+#define LSM6DS33_OUTY_L_XL          0x2a
+#define LSM6DS33_OUTY_H_XL          0x2b
+#define LSM6DS33_OUTZ_L_XL          0x2c
+#define LSM6DS33_OUTZ_H_XL          0x2d
+
+#define LSM6DS33_FIFO_STATUS1       0x3a
+#define LSM6DS33_FIFO_STATUS2       0x3b
+#define LSM6DS33_FIFO_STATUS3       0x3c
+#define LSM6DS33_FIFO_STATUS4       0x3d
+#define LSM6DS33_FIFO_DATA_OUT_L    0x3e
+#define LSM6DS33_FIFO_DATA_OUT_H    0x3f
+
+#define LSM6DS33_TIMESTAMP0_REG     0x40
+#define LSM6DS33_TIMESTAMP1_REG     0x41
+#define LSM6DS33_TIMESTAMP2_REG     0x42
+
+#define LSM6DS33_STEP_TIMESTAMP_L   0x49
+#define LSM6DS33_STEP_TIMESTAMP_H   0x4a
+#define LSM6DS33_STEP_COUNTER_L     0x4b
+#define LSM6DS33_STEP_COUNTER_H     0x4c
+
+#define LSM6DS33_FUNC_SRC           0x53
+
+#define LSM6DS33_TAP_CFG            0x58
+#define LSM6DS33_TAP_THS_6D         0x59
+#define LSM6DS33_INT_DUR2           0x5a
+#define LSM6DS33_WAKE_UP_THS        0x5b
+#define LSM6DS33_WAKE_UP_DUR        0x5c
+#define LSM6DS33_FREE_FALL          0x5d
+#define LSM6DS33_MD1_CFG            0x5e
+#define LSM6DS33_MD2_CFG            0x5f
+
+//  Gyro sample rate defines
+
+#define LSM6DS33_GYRO_SAMPLERATE_13   0x00
+#define LSM6DS33_GYRO_SAMPLERATE_26   0x01
+#define LSM6DS33_GYRO_SAMPLERATE_52   0x02
+#define LSM6DS33_GYRO_SAMPLERATE_104  0x03
+#define LSM6DS33_GYRO_SAMPLERATE_208  0x04
+#define LSM6DS33_GYRO_SAMPLERATE_416  0x05
+#define LSM6DS33_GYRO_SAMPLERATE_833  0x06
+#define LSM6DS33_GYRO_SAMPLERATE_1660 0x07
+
+//  Gyro FSR defines
+
+#define LSM6DS33_GYRO_FSR_125       0x00
+#define LSM6DS33_GYRO_FSR_245       0x01
+#define LSM6DS33_GYRO_FSR_500       0x02
+#define LSM6DS33_GYRO_FSR_1000      0x03
+#define LSM6DS33_GYRO_FSR_2000      0x04
+
+//  Gyro high pass filter defines
+
+#define LSM6DS33_GYRO_HPF_0         0x00
+#define LSM6DS33_GYRO_HPF_1         0x01
+#define LSM6DS33_GYRO_HPF_2         0x02
+#define LSM6DS33_GYRO_HPF_3         0x03
+
+//  Accel sample rate defines
+
+#define LSM6DS33_ACCEL_SAMPLERATE_13   0x00
+#define LSM6DS33_ACCEL_SAMPLERATE_26   0x01
+#define LSM6DS33_ACCEL_SAMPLERATE_52   0x02
+#define LSM6DS33_ACCEL_SAMPLERATE_104  0x03
+#define LSM6DS33_ACCEL_SAMPLERATE_208  0x04
+#define LSM6DS33_ACCEL_SAMPLERATE_416  0x05
+#define LSM6DS33_ACCEL_SAMPLERATE_833  0x06
+#define LSM6DS33_ACCEL_SAMPLERATE_1660 0x07
+#define LSM6DS33_ACCEL_SAMPLERATE_3330 0x08
+#define LSM6DS33_ACCEL_SAMPLERATE_6660 0x09
+
+//  Accel FSR defines
+
+#define LSM6DS33_ACCEL_FSR_2        0x00
+#define LSM6DS33_ACCEL_FSR_4        0x01
+#define LSM6DS33_ACCEL_FSR_8        0x02
+#define LSM6DS33_ACCEL_FSR_16       0x03
+
+// Accel low pass filter defines
+
+#define LSM6DS33_ACCEL_LPF_400      0x00
+#define LSM6DS33_ACCEL_LPF_200      0x01
+#define LSM6DS33_ACCEL_LPF_100      0x02
+#define LSM6DS33_ACCEL_LPF_50       0x03
+
+//----------------------------------------------------------
+//
+//  LIS3MDL
+
+//  I2C Slave Addresses
+#define LIS3MDL_ADDRESS0            0x1c
+#define LIS3MDL_ADDRESS1            0x1e
+#define LIS3MDL_ID                  0x3d
+
+//  LIS3MDL Register map
+#define LIS3MDL_WHO_AM_I            0x0f
+
+#define LIS3MDL_CTRL_REG1           0x20
+#define LIS3MDL_CTRL_REG2           0x21
+#define LIS3MDL_CTRL_REG3           0x22
+#define LIS3MDL_CTRL_REG4           0x23
+#define LIS3MDL_CTRL_REG5           0x24
+
+#define LIS3MDL_STATUS_REG          0x27
+
+#define LIS3MDL_OUT_X_L             0x28
+#define LIS3MDL_OUT_X_H             0x29
+#define LIS3MDL_OUT_Y_L             0x2a
+#define LIS3MDL_OUT_Y_H             0x2b
+#define LIS3MDL_OUT_Z_L             0x2c
+#define LIS3MDL_OUT_Z_H             0x2d
+
+#define LIS3MDL_TEMP_OUT_L          0x2e
+#define LIS3MDL_TEMP_OUT_H          0x2f
+
+#define LIS3MDL_INT_CFG             0x30
+#define LIS3MDL_INT_SRC             0x31
+#define LIS3MDL_INT_THS_L           0x32
+#define LIS3MDL_INT_THS_H           0x33
+
+//  Sample rates
+#define LIS3MDL_SAMPLERATE_0_625    0x00
+#define LIS3MDL_SAMPLERATE_1_25     0x01
+#define LIS3MDL_SAMPLERATE_2_5      0x02
+#define LIS3MDL_SAMPLERATE_5        0x03
+#define LIS3MDL_SAMPLERATE_10       0x04
+#define LIS3MDL_SAMPLERATE_20       0x05
+#define LIS3MDL_SAMPLERATE_40       0x06
+#define LIS3MDL_SAMPLERATE_80       0x07
+#define LIS3MDL_SAMPLERATE_FAST     0x08
+
+//  FSR
+#define LIS3MDL_FSR_4               0x00
+#define LIS3MDL_FSR_8               0x01
+#define LIS3MDL_FSR_12              0x02
+#define LIS3MDL_FSR_16              0x03
+
+// Power modes
+#define LIS3MDL_POWER_LP            0x00
+#define LIS3MDL_POWER_MP            0x01
+#define LIS3MDL_POWER_HP            0x02
+#define LIS3MDL_POWER_UHP           0x03
 
 #endif // _RTIMUDEFS_H
